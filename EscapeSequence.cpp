6d197f9e89af6b5e0be6f65670853815162f6903
mdOwaisAnsary <105032694+mdOwaisAnsary@users.noreply.github.com>
2024-10-23 12:32:19 +0500

    Add files via upload

---
+++ EscapeSequence.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	/*
+	Escape Sequence
+	\n
+	\t
+	\a
+	\'
+	*/
+	
+	cout << "This\nis\na\ntest\n\nShe said, \"Sells she seashells on the seashore?\"\n";
+	
+	cout<<"\\n";
+	
+	
+}
---
+++ EscapeSequenceExample.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	int a, b, s;
+	
+	cout<<"first:"<<endl;
+	cin>>a;
+	cout<<"second:"<<endl;
+	cin>>b;
+	cout<<"\n";
+	cout<<a+b;
+	/*
+	Escape Sequence
+	\n
+	\t
+	\a
+	\'
+	*/
+	
+	//cout << "\"C++\" was developed by \'Bjarne Stroustrup\', \n as an extension to the \'C language\'.\n In \"1985\", the first edition of The C++ Programming Language was released,\n it has Datatypes , varaible escape sequences such as\n \'\\n\' & \"\\t\" ";
+	
+	//cout<<" \"C++\" was developed by denis ritche,\n and extension of \'C\',\n In \"1985 the first edition of C++ Prgramming was released,\n it has DT , Var and ES such as\n \'\\n & t";
+	
+}
